9dfa5cd913960bd3110106e0951914e518262027
Teewiretech <taiwomutiu1998@yahoo.com>
2023-06-27 11:26:37 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,6 +19,7 @@ len++;
 for (index = len - 1; index >= len / 2; index--)
 {
 tmp = s[index];
-s[index] = s[len - index - 1];s[len - index - 1] = tmp;
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
 }
 }
