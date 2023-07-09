54f8903fd2cb7cbb348f182a8a44243287f866ce
Teewiretech <taiwomutiu1998@yahoo.com>
2023-07-09 03:27:46 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char *_memcpy(char *dest, char *src, char  *n)
+{
+	return (dest, src, n);
+}
+
